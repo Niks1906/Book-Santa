512f62fec07879faa2903edda3c1e3ba2dfb745c
Niks1906 <66248757+Niks1906@users.noreply.github.com>
2021-04-21 17:57:00 +0530

    Add files via upload

--- Screens/authScreen.js
+++ Screens/authScreen.js
@@ -3,10 +3,12 @@ import {
   View,
   Text,
   TouchableOpacity,
-  StyleSheet,
   TextInput,
   Alert,
-  TouchableWithoutFeedbackBase,
+  Modal,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as firebase from "firebase";
@@ -15,7 +17,15 @@ import db from "../config";
 export default class Authentication extends React.Component {
   constructor() {
     super();
-    this.state = { email: "", password: "" };
+    this.state = {
+      email: "",
+      password: "",
+      isModal: false,
+      name: "",
+      address: "",
+      contact: "",
+      confirmPassword: "",
+    };
   }
 
   logIn = () => {
@@ -36,13 +46,131 @@ export default class Authentication extends React.Component {
         Alert.alert(errorCode);
       }
     } else {
-      Alert.alert("Please enter email and password");
+      return Alert.alert("Please enter email and password");
     }
+  };
+
+  userSignUp = () => {
+    if (this.state.password === this.state.confirmPassword) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => {
+          db.collection("users").add({
+            name: this.state.name,
+            address: this.state.address,
+            contact: this.state.contact,
+            email: this.state.email,
+          });
+          return Alert.alert(
+            "User added successfully",
+            "Please Login to continue",
+            [
+              {
+                text: "OK",
+                onPress: () => {
+                  this.setState({ isModal: false });
+                },
+              },
+            ]
+          );
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          return Alert.alert(errorMessage);
+        });
+    } else {
+      return Alert.alert("Passwords do not match");
+    }
+  };
+
+  showModal = () => {
+    return (
+      <Modal
+        visible={this.state.isModal}
+        transparent={true}
+        animationType={"fade"}
+      >
+        <View style={styles.view}>
+          <KeyboardAvoidingView>
+            <ScrollView>
+              <Text>Sign Up</Text>
+              <TextInput
+                placeholder="Enter your name"
+                style={styles.textInput}
+                onChangeText={(text) => {
+                  this.setState({ name: text });
+                }}
+              />
+              <TextInput
+                placeholder="Enter your address"
+                style={styles.textInput}
+                onChangeText={(text) => {
+                  this.setState({ address: text });
+                }}
+              />
+              <TextInput
+                placeholder="Enter your phone number"
+                style={styles.textInput}
+                onChangeText={(text) => {
+                  this.setState({ contact: text });
+                }}
+                keyboardType="number"
+              />
+              <TextInput
+                placeholder="Enter your e-mail id"
+                style={styles.textInput}
+                onChangeText={(text) => {
+                  this.setState({ email: text });
+                }}
+                keyboardType="email"
+              />
+              <TextInput
+                placeholder="Enter your password"
+                style={styles.textInput}
+                onChangeText={(text) => {
+                  this.setState({ password: text });
+                }}
+                secureTextEntry={true}
+              />
+              <TextInput
+                placeholder="Confirm your password"
+                style={styles.textInput}
+                onChangeText={(text) => {
+                  this.setState({ confirmPassword: text });
+                }}
+              />
+              <TouchableOpacity
+                onPress={() => {
+                  this.userSignUp();
+                }}
+              >
+                <Text>Sign Up</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  this.setState({ isModal: false });
+                }}
+              >
+                <Text>Cancel</Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </KeyboardAvoidingView>
+        </View>
+      </Modal>
+    );
   };
 
   render() {
     return (
-      <SafeAreaView>
+      <SafeAreaView
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View>{this.showModal()}</View>
         <TextInput
           style={{
             backgroundColor: "yellow",
@@ -72,7 +200,29 @@ export default class Authentication extends React.Component {
         >
           <Text>Log In</Text>
         </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            this.setState({ isModal: true });
+          }}
+        >
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: "yellow",
+    borderWidth: 2,
+    borderColor: "brown",
+  },
+  view: {
+    flex: 1,
+    backgroundColor: 'black',
+    margin:30,
+  }
+});
