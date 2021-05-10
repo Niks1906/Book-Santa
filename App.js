fa42ed84f3f288e185db7ede5d3a822cf89ecf97
Niks1906 <66248757+Niks1906@users.noreply.github.com>
2021-05-10 17:58:48 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import * as React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import { createBottomTabNavigator } from "react-navigation-tabs";
-import Request from "./Screens/requestScreen";
-import Share from "./Screens/shareScreen";
 import Authentication from "./Screens/authScreen";
 import { AppDrawer } from "./components/drawerNav";
 import RecieverDetails from "./Screens/recieverDetails";
--- Screens/currentUserDonation.js
+++ Screens/currentUserDonation.js
@@ -1,6 +1,8 @@
 import * as React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, FlatList, ScrollView, View } from "react-native";
 import * as firebase from "firebase";
+import db from "../config";
+import { Icon, Card, ListItem } from "react-native-elements";
 
 export default class CurrentUserDonations extends React.Component {
   constructor() {
@@ -25,17 +27,85 @@ export default class CurrentUserDonations extends React.Component {
       });
   };
 
-  getAllDonations = () => {};
+  getAllDonations = () => {
+    db.collection("donations")
+      .where("donorID", "==", this.state.currentUserID)
+      .onSnapshot((snapshot) => {
+        var allDonations = [];
+        snapshot.docs.map((doc) => {
+          var donation = doc.data();
+          donation["docID"] = doc.ID;
+          allDonations.push(donation);
+        });
+        this.setState({ allDonations: allDonations });
+      });
+  };
 
   componentDidMount() {
     this.getCurrentUserDetails();
     this.getAllDonations();
   }
 
+  sendNotification = (bookDetails) => {
+    var message =
+      this.state.currentUserName + " has sent you book " + bookDetails.bookName;
+    db.collection("notifications")
+      .where("requestID", "==", bookDetails.requestID)
+      .where("donorID", "==", bookDetails.donorID)
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          db.collection("notifications").doc(doc.id).update({
+            message: message,
+            date: firebase.firestore.FieldValue.serverTimestamp(),
+            status: "unread",
+          });
+        });
+      });
+  };
+
+  sendBook = (bookDetails) => {
+    if (bookDetails.status === "donor interested") {
+      db.collection("donations").doc(bookDetails.docID).update({
+        status: "book sent",
+      });
+      this.sendNotification(bookDetails);
+    }
+  };
+
+  keyExtractor = (item, index) => index.toString();
+  renderItem = (item, i) => {
+    return (
+      <ListItem key={i} bottomDivider="true">
+        <Text>{item.bookName}</Text>
+        <Text>
+          {"Requested by" + item.recieverID + " Status:" + item.status}
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            this.sendBook(item);
+          }}
+        >
+          <Text>Donate</Text>
+        </TouchableOpacity>
+      </ListItem>
+    );
+  };
+
   render() {
     return (
       <SafeAreaView>
-        <Text>my current donations</Text>
+        {this.state.allDonations.length === 0 ? (
+          <View>
+            <Text>You have no donations</Text>
+          </View>
+        ) : (
+          <FlatList
+            keyExtractor={this.keyExtractor}
+            renderItem={this.renderItem}
+            data={this.state.allDonations}
+          />
+        )}
       </SafeAreaView>
     );
   }
